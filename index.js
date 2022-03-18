fc64842b8301f212b323c1f66558a5afbd56ec24
Andreas Kohn <andreas.kohn@gmail.com>
2022-03-18 14:35:59 +0100

    Reimplement for the new "variables resolution mode"
    While there: Switch from using `getValueFromOptions` to directly querying the
    serverless options.
    
    Together-with: @eacet
    
    See https://www.serverless.com/framework/docs/deprecations/#NEW_VARIABLES_RESOLVER

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 'use strict';
 
+/** @type {import('serverless/classes/Plugin')} */
 class RequiredOptionsServerlessPlugin {
-	constructor(serverless) {
-		this.variableResolvers = {
-			requiredOpt: async variableString => {
-				// In serverless 2.16.1 getValueFromOptions will just ignore the prefix and pick the second part after the ':', but
-				// for added sanity we change the string to match what it should be.
-				// Note that we're not responsible for any recursive resolution, and simply behave like "opt-but-with-error"
-				const [, option] = variableString.split(/:/);
-				const value = await serverless.variables.getValueFromOptions(`opt:${option}`);
-				if (value) {
-					return value;
-				}
-				throw new Error(`Required option "${option}" not provided`);
-			}
+	/**
+	 *
+	 * @param {import('serverless')} _serverless
+	 * @param {import('serverless').Options} options
+	 */
+	 constructor(_serverless, options) {
+		this.configurationVariablesSources = {
+			requiredOpt: {
+				async resolve({ address }) {
+					const value = options[address];
+					if (value) {
+						return { value };
+					}
+					throw new Error(`Required option "${address}" not provided`);
+				},
+			},
 		}
 	}
 }
